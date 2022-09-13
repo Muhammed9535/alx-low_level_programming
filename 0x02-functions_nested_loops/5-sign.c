b5d573341bdaca021bd177bc69bb7bbe11adc425
lawal olarewaju <lawalolarewaju123@gmail.com>
2022-09-13 15:34:56 -0700

    6th commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- *print _sign - function to the sign of a number
+ * print_sign - function to the sign of a number
  * @n: is the int that will use for the argument of the function
  * Return:0
  */
