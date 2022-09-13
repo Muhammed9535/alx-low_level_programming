274651cfd4be9a363d6bba9d20985608ba78ecbf
lawal olarewaju <lawalolarewaju123@gmail.com>
2022-09-13 12:50:05 -0700

    2nd commit

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - entry point
+ *
+ * Return:0
+ */
+
+int main(void)
+{
+	int c;
+
+	for (c = 'a'; c <= 'z'; c++)
+		_putchar(c);
+	
+	_putchar('\n');
+	return (0);
+}
