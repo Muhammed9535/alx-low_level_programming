e104367dbc86bdb6360eeb6c752a2faf0b1010e6
lawal olarewaju <lawalolarewaju123@gmail.com>
2022-09-21 23:25:05 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -9,12 +9,16 @@
 
 void reverse_array(int *a, int n)
 {
-	int tmp, index;
+	int x = 0;
+	int y = n - 1;
+	int tmp;
 
-	for (index = n - 1; index >= n / 2; index--)
+	while (x < y)
 	{
-		tmp = a[n - 1 - index];
-		a[n - 1 - index] = a[index];
-		a[index] = tmp;
+		tmp = a[x];
+		a[x] = a[y];
+		a[y] = tmp;
+		x++;
+		y--;
 	}
 }
