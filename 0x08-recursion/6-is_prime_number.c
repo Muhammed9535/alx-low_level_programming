ab1ceb1fa86577573a45c6b947c92f1a131aa284
lawal olarewaju <lawalolarewaju123@gmail.com>
2022-09-28 14:14:12 -0700

    2nd commit

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,7 +5,7 @@ int is_prime_number(int n);
 
 
 /**
- * is_prime_number - Checks if a number is divisible.
+ * is_divisible - Checks if a number is divisible.
  * @num: The number to be checked.
  * @div: The divisor.
  *
