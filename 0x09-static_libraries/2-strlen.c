71dca9f95148bde7347402f9c98462a119151e88
lawal olarewaju <lawalolarewaju123@gmail.com>
2022-10-01 03:43:54 -0700

    first commit

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string
+ * @s: the string to get the length
+ *
+ * Return: thr length of *s
+ */
+
+int _strlen(char *s)
+{
+	int length = 0;
+
+	while (*s++)
+		length++;
+
+	return (length);
+}
