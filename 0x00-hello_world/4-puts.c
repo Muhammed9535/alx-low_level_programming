126b072455c4aa9d6024b4efd488b5d9d3f16259
Muhammed9535 <lawalolarewaju123@gmai.com>
2023-12-14 10:04:44 -0800

    5th commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main- print out a function
+ * description: A function that print out a string
+ * using puts() function.
+ *
+ * Return: 0.
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle\n");
+	return (0);
+}
