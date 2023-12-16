879ace45d94cdd2cca22d96eab6cd2ff1e241282
Muhammed9535 <lawalolarewaju123@gmai.com>
2023-12-15 16:48:40 -0800

    3rd commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -2,24 +2,23 @@
 /**
   * main -  prints all possible different combinations of two digits
   * Description: Write a program that prints all possible different
-  combinations of two digits separated by , followed by a space
+  * combinations of two digits separated by , followed by a space
 * Return: 0
 */
 int main(void)
 {
-	int i, j, c;
+	int i, j;
 
 	for (i = 0; i <= 8; i++)
 	{
-		for (j = 1; j <= 9; j++){
-			putchar(i + '0');
-			putchar(j + '0');
-			c = i + j;
-			if (c < 15)
-			{
-				putchar(',');
-				putchar(' ');
-			}
+		for (j =  i + 1; j <= 9; j++)
+		{
+			putchar((i % 10) + '0');
+			putchar((j % 10) + '0');
+			if (i == 8 && j == 9)
+				continue;
+			putchar(',');
+			putchar(' ');
 		}
 	}
 	putchar('\n');
--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,9 +11,9 @@ int main(void)
 
 	for (i = 0; i <= 7; i++)
 	{
-		for (j = 1; j <= 8; j++)
+		for (j = i + 1; j <= 8; j++)
 		{
-			for (k = 2; k <= 9; k++)
+			for (k = j + 1; k <= 9; k++)
 			{
 				if (i != j && i != k && j != k)
 				{
@@ -27,9 +27,9 @@ int main(void)
 						putchar(' ');
 					}
 				}
-				
 			}
 		}
 	}
+	putchar('\n');
 	return (0);
 }
