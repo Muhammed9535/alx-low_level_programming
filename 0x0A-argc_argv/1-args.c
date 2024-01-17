3c90e7387db342665d6a506a98c1a66fd01f27b7
Muhammed9535 <lawalolarewaju123@gmai.com>
2024-01-17 13:26:30 -0800

    second commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,7 +2,8 @@
 
 /**
   * main -  a program that prints the number of arguments passed into it.
-  * Description:  a program that prints the number of arguments passed into it.
+  * @argc:  argument count.
+  * @argv: pointer to character
   * Return: 0
   */
 int main(int argc, char __attribute__((__unused__)) *argv[])
