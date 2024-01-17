61ffa9a444e52571c60341ac479368fef2fb4aa0
Muhammed9535 <lawalolarewaju123@gmai.com>
2024-01-17 13:34:53 -0800

    third commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+  * main - print the program name
+  * @argc: argument count
+  * @argv: pointer to character
+  * Return: ALways 0
+  */
+int main(int __attribute__((__unused__)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
