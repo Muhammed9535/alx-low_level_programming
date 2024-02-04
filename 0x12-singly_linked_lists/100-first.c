89537ab51860d1e14fe2f8bfe52791d5d8d09463
Muhammed9535 <lawalolarewaju123@gmail.com>
2024-02-04 00:22:44 -0800

    last commit

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+  * my_constructor - print content before the main function
+  * Return: nothing
+  */
+
+void my_constructor(void)
+{
+	printf("You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n");
+}
