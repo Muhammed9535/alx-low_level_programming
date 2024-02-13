203b4d5c2250c18d08600bc96cb52d56e78c442d
Muhammed9535 <lawalolarewaju123@gmail.com>
2024-02-13 01:09:31 -0800

    second commit

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,16 +17,20 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	fd = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+
+	fd = open(filename, O_CREAT |O_RDWR | O_TRUNC, 0600);
 
 	if (fd == -1)
 		return (-1);
 
-	bytes_read = write(fd, text_content, strlen(text_content));
-	if (bytes_read == -1)
+	if (text_content != NULL)
 	{
-		close(fd);
-		return (-1);
+		bytes_read = write(fd, text_content, strlen(text_content));
+		if (bytes_read == -1)
+		{
+			close(fd);
+			return (-1);
+		}
 	}
 
 	if (close(fd) == -1)
